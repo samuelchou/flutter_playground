c3d458b4d2d1897413c6a101a538174387e97ac8
Samuel Chou <samuelchou@hotmail.com.tw>
2023-02-28 15:10:42 +0800

    feat(input-field): initial tests with text input.

---
+++ lib/input_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Lab Code"),
+      ),
+      body: Center(
+        child: HomePage(),
+      ),
+    ),
+  );
+  runApp(app);
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        TextField(
+          decoration: const InputDecoration(hintText: "Input your name here"),
+          controller: textEditingController,
+        ),
+        ElevatedButton(
+          onPressed: saidHelloWithInput,
+          child: const Text("Say Hello"),
+        ),
+      ],
+    );
+  }
+
+  void saidHelloWithInput() {
+    debugPrint("Hello ${textEditingController.text}");
+  }
+}
