8f25f1aad0c300c5c4c77523823591f3b2ed91b1
Samuel Chou <samuelchou@hotmail.com.tw>
2023-03-22 19:58:55 +0800

    feat(shared-preferences): setup the environment.

---
+++ lib/shared_preferences.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Lab Code"),
+      ),
+      body: Center(
+        child: HomePage(),
+      ),
+    ),
+  );
+  runApp(app);
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: replace with shared pref.
+    textEditingController.text = "Samuel";
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        TextField(
+          decoration: const InputDecoration(
+            hintText: "Input your name here",
+            prefixIcon: Icon(Icons.abc),
+          ),
+          keyboardType: TextInputType.number,
+          controller: textEditingController,
+        ),
+        ElevatedButton(
+          onPressed: () => saidHelloWithInput(context),
+          child: const Text("Say Hello"),
+        ),
+      ],
+    );
+  }
+
+  void saidHelloWithInput(BuildContext context) {
+    final input = textEditingController.text;
+    debugPrint('Hello $input');
+    final snackBar = SnackBar(content: Text('Hello $input'));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
